641c4502e07886de81a3b5c77280135a502ed123
Fluke <tawinp57@email.nu.ac.th>
2019-11-10 00:56:45 +0700

    update connect db

--- index.php
+++ index.php
@@ -4,28 +4,74 @@
     <body>
         <div class="content" >
             <div class="content-add" >
-                    <div class="row" >
+                <div class="row" >
+                    <div class="col-lg-12 col-md-12  col-sm-12 header-text">
                         <span class=""> Manage Hotel</span>
                     </div>
-                <button type="button" class="btn btn-primary">Add Hotel</button>
+                </div>
+                <div class="row" >
+                    <div class="col-lg-12 col-md-12  col-sm-12 ">
+                        <button type="button" class="btn btn-primary">Add Hotel</button>
+                    </div>
+                </div>
             </div>
             <div class="content-table" >
                 <table class="table">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">Hotel Name</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Picture</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
+                            <td>hotel name1</td>
+                            <td>show hotel</td>
+                            <td>
+                                <a href="#" target="_blank">
+                                    img_name
+                                </a> 
+                            </td>
+                            <td>
+                                <a href="#" >
+                                    <i class="fas fa-edit"></i>
+                                </a> 
+                                <a href="" >
+                                    <i class="far fa-trash-alt"></i>
+                                </a> 
+                            </td>
                         </tr>
+                        <?php 	
+                            $sql = "SELECT * FROM hotel " ;
+                            $rs = $mysqli->query($sql);
+                            $items = array();
+                            while($hotel = $rs->fetch_assoc()){
+                        ?>
+                            <tr>
+                                <th scope="row"></th>
+                                <td><?php echo $hotel['hotel_name']; ?></td>
+                                <td><?php echo $hotel['hotel_description']; ?></td>
+                                <td>
+                                    <a href="#" target="_blank">
+                                        <?php echo $hotel['picture']; ?>
+                                    </a> 
+                                </td>
+                                <td>
+                                    <a href="#" >
+                                        <i class="fas fa-edit"></i>
+                                    </a> 
+                                    <a href="" >
+                                        <i class="far fa-trash-alt"></i>
+                                    </a> 
+                                </td>
+                            </tr>
+                        <?php     
+                           }
+                        ?>
                     </tbody>
                 </table>
             </div>
