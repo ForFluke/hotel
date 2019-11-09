06ce684dcfa18671c04428ad1dd416222065103d
Fluke <tawinp57@email.nu.ac.th>
2019-11-10 00:25:19 +0700

    update ui manage hotel

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
-<?php 
-    echo 'ok';
-?>
+<!DOCTYPE html>
+<html>
+    <?php include 'header.php'; ?>
+    <body>
+        <div class="content" >
+            <div class="content-add" >
+                    <div class="row" >
+                        <span class=""> Manage Hotel</span>
+                    </div>
+                <button type="button" class="btn btn-primary">Add Hotel</button>
+            </div>
+            <div class="content-table" >
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
